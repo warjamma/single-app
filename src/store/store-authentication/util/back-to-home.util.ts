8c5e1e87175c728c2d3abb0c8e6b778b557bd74a
mao nguyen <lgold141@gmail.com>
2024-02-04 23:37:20 +0700

    fix: navigate auth

--- src/store/store-authentication/util/back-to-home.util.ts
+++ src/store/store-authentication/util/back-to-home.util.ts
@@ -1,10 +1,10 @@
 import { AppHistory } from '../../store-router-manager';
 
 export const backToHome = (delay?: number) => {
-  if (!delay || delay < 0) AppHistory(). navigate('/');
+  if (!delay || delay < 0) AppHistory().navigate('/');
   else {
     setTimeout(() => {
-      AppHistory(). navigate('/');
+      AppHistory().navigate('/');
     }, delay);
   }
 };
