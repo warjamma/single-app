826ec3e0571b934e535c1d4255c5927d838b8d2e
mao nguyen <lgold141@gmail.com>
2024-03-03 17:42:06 +0700

    chore: add SEO notification page

--- src/pages/notification/notification.page.tsx
+++ src/pages/notification/notification.page.tsx
@@ -4,6 +4,7 @@ import { NotificationItem } from './components';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { LoadingMore } from '../../components';
 import { useNotification } from './notification.hook';
+import { Helmet } from 'react-helmet-async';
 
 export const NotificationPage: React.FC<INotificationPageProps> = (props) => {
   const { isDropdown } = props;
@@ -12,6 +13,10 @@ export const NotificationPage: React.FC<INotificationPageProps> = (props) => {
 
   return (
     <div className={`notification-page flex justify-center py-6 ${isDropdown ? '' : 'pt-20'}`}>
+      <Helmet>
+        <title>Notification Page</title>
+        <meta name="home" content="Notification Page" />
+      </Helmet>
       <div className={`w-[600px] bg-white rounded-md ${isDropdown ? '' : 'shadow shadow-slate-250'}`}>
         <InfiniteScroll
           dataLength={notificationItems?.length || 0}
